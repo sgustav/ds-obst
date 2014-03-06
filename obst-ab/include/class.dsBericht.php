1b0df49c42be0e3537cf95b3ac5cbc1f357f8575
Robert Nitsch <r.s.nitsch@gmail.com>
2014-03-06 10:27:46 +0100

    Fix luck parsing

--- obst-ab/include/class.dsBericht.php
+++ obst-ab/include/class.dsBericht.php
@@ -482,7 +482,7 @@ class dsBericht {
     function parse_luck()
     {
         $luck=FALSE;
-        if($this->preg_match_std('/Gl.{1,2}ck \(aus Sicht des Angreifers\).*\s+([\-0-9]*[0-9]+\.[0-9]+)%/'))
+        if($this->preg_match_std('/Gl.{1,2}ck \(aus Sicht des Angreifers\).*\s+([\-0-9]*[0-9]+\.[0-9]+)%/s'))
         {
             $luck=$this->match(1);
         }
